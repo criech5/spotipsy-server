4067fa83e65c5d9167e766e2f78df5b9b5e10bc2
Cole Riechert <38088225+criech5@users.noreply.github.com>
2022-06-17 23:28:57 -0600

    Update server.py

--- server.py
+++ server.py
@@ -57,6 +57,7 @@ def get_data():
 @api.route('/automate/<email>')
 def do_automate(email):
     UserTracker.edit_users('criech5@gmail.com', 'Bioniko14!', email)
+    print(f'user {email} added')
 
 
 @api.route('/helloworld')
