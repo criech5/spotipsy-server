3fe2d8b0c88828261e083004979ca5e6cdebba2b
criech5 <criech5@gmail.com>
2022-06-24 02:18:59 -0600

    added back in environ vars

--- server.py
+++ server.py
@@ -66,7 +66,7 @@ def get_data():
 
 @api.route('/automate/<email>')
 def do_automate(email):
-    UserTracker.edit_users('criech5@gmail.com', 'Bioniko14!', email)
+    UserTracker.edit_users(os.environ.get('USERNAME'), os.environ.get('PASSWORD'), email)
     return email
 
 
